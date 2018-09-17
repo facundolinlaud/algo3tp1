569ac13a25c9f6922ef7e176eff181b404362e30
Facundo Linlaud <facundolinlaud@gmail.com>
2018-09-16 21:23:01 -0300

    casi cerrando

--- experimentacion/params/params.py
+++ experimentacion/params/params.py
@@ -1,10 +1,12 @@
 {
 	'exe' : '../impl/cmake-build-debug/impl',
 	'n_start' : 10,
-	'n_end' : 30,
+	'n_end' : 20,
 	'n_step' : 1,
 	'repetitions_per_n' : 10,
 	'include_unsolvable_problems' : True,
+	'shuffle_problems' : True,
+	'solutions_at_tail' : True,
 	'runs' : {
 		'Brute-Force' : {
 			'algorithm' : 'bf'
--- experimentacion/params/params_no_shuffling.py
+++ experimentacion/params/params_no_shuffling.py
@@ -1,10 +1,12 @@
 {
 	'exe' : '../impl/cmake-build-debug/impl',
 	'n_start' : 10,
-	'n_end' : 30,
+	'n_end' : 20,
 	'n_step' : 1,
-	'repetitions_per_n' : 40,
-	'include_unsolvable_problems' : True,
+	'repetitions_per_n' : 2,
+	'include_unsolvable_problems' : False,
+	'shuffle_problems' : False,
+	'solutions_at_tail' : True,
 	'runs' : {
 		'Brute-Force' : {
 			'algorithm' : 'bf'
